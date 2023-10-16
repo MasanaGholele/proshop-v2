d1037520050293703a155776c9dd8216a48689c9
Masana <masanam.rikhotso@gmail.com>
2023-10-16 09:40:12 +0200

    first commit

---
+++ frontend/src/index.js
@@ -0,0 +1,41 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+
+// import 'bootstrap/dist/css/bootstrap.min.css'; // we will use the custom bootstrap (below) css file instead
+// import './index.css'; // we will use the custom bootstrap (below) css file instead
+import "./assets/styles/bootstrap.custom.css";
+import "./assets/styles/index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+      <Route index={true} path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
+
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+    <RouterProvider router={router} />
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
