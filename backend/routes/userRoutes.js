a52a59f87955c4cad1de163f79f10bd9d78e31f7
Masana <masanam.rikhotso@gmail.com>
2023-10-22 16:36:51 +0200

    user routes

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -13,7 +13,7 @@ import {
 } from '../controllers/userController.js';
 
 router.route('/').post(registerUser).get(getUsers);
-router.post('/logout');
+router.post('/logout', logoutUser);
 router.post('/login', authUser);
 router.route('/profile').get(getUserProfile).put(updateUserProfile);
 router.route('/:id').delete(deleteUser).get(getUserById).put(updateUser);
