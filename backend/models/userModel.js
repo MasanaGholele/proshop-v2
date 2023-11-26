994252f5ad14000e0d5a1b5d47e3506830e53424
Masana <masanam.rikhotso@gmail.com>
2023-11-26 09:25:26 +0200

    user endpoint encryption

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -31,6 +31,14 @@ userSchema.methods.matchPassword = async function (enteredPassword) {  // compar
   return await bcrypt.compare(enteredPassword, this.password);  // this.password is the hashed password
 };
 
+userSchema.pre('save', async function (next) { // .pre ensures that it is done before saving to the database
+  if (!this.isModified('password')) { // if password is not modified it will move on to the next function
+    next();
+  }
+  const salt = await bcrypt.genSalt(10);
+this.password = await bcrypt.hash(this.password, salt); // if password is modified then we hash if before saving to the DB
+});
+
 const User = mongoose.model('User', userSchema);
 
 export default User;
