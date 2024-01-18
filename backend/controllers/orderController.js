4f91e6ceead4cae0e69179ea7b9c6bda8224f218
Masana <masanam.rikhotso@gmail.com>
2024-01-18 11:43:38 +0200

    paypal intergration

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -44,21 +44,24 @@ const addOrderItems = asyncHandler(async (req, res) => {
 // @route   GET /api/orders/myorders
 // @access  Private
 const getMyOrders = asyncHandler(async (req, res) => {
-const orders = await Order.find({ user: req.user._id });
-res.status(200).json(orders);
+  const orders = await Order.find({ user: req.user._id });
+  res.status(200).json(orders);
 });
 
 // @desc   Get order by ID
 // @route   GET /api/orders/:id
 // @access  Private
 const getOrderById = asyncHandler(async (req, res) => {
-  const order = await Order.findById(req.params.id).populate('user', 'name email');
+  const order = await Order.findById(req.params.id).populate(
+    "user",
+    "name email"
+  );
 
   if (order) {
     res.status(200).json(order);
   } else {
     res.status(404);
-    throw new Error('Order not found');
+    throw new Error("Order not found");
   }
 });
 
@@ -66,9 +69,24 @@ const getOrderById = asyncHandler(async (req, res) => {
 // @route   PUT /api/orders/:id/pay
 // @access  Private
 const updateOrderToPaid = asyncHandler(async (req, res) => {
-  res.send("update order to paid");
-});
+  const order = await Order.findById(req.params.id);
 
+  if (order) {
+    (order.isPaid = true), (order.isPaidAt = Date.now());
+    order.paymentResult = {
+      id: req.body.id,
+      status: req.body.status,
+      update_time: req.body.update_time,
+      email_address: req.body.payer.email_address,
+    };
+    const updatedOrder = await order.save();
+
+    res.status(200).json(updatedOrder);
+  } else {
+    res.status(404);
+    throw new Error('Order not found');
+  }
+});
 // @desc   Update order to delivered
 // @route   PUT /api/orders/:id/deliver
 // @access  Private/Admin
--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
+import express from "express";
+import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 dotenv.config();
-import connectDB from './config/db.js';
-import productRoutes from './routes/productRoutes.js';
-import userRoutes from './routes/userRoutes.js';
-import orderRoutes from './routes/orderRoutes.js';
-import { notFound, errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from "./config/db.js";
+import productRoutes from "./routes/productRoutes.js";
+import userRoutes from "./routes/userRoutes.js";
+import orderRoutes from "./routes/orderRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
-const port = process.env.PORT || 5000; 
+const port = process.env.PORT || 5000;
 
-connectDB();   // connect to MongoDB (call function from db.js)
+connectDB(); // connect to MongoDB (call function from db.js)
 
 const app = express();
 
@@ -21,16 +21,21 @@ app.use(express.urlencoded({ extended: true }));
 // Cookie parser middleware
 app.use(cookieParser());
 
-app.get('/', (req, res) => {            // "home page route"
-    res.send('API is running...');
+app.get("/", (req, res) => {
+  // "home page route"
+  res.send("API is running...");
 });
 
-app.use('/api/products', productRoutes);   // "use" the productRoutes
-app.use('/api/users', userRoutes);         // "use" the userRoutes
-app.use('/api/orders', orderRoutes);         // "use" the orderRoutes
+app.use("/api/products", productRoutes); // "use" the productRoutes
+app.use("/api/users", userRoutes); // "use" the userRoutes
+app.use("/api/orders", orderRoutes); // "use" the orderRoutes
 
-app.use(notFound);      // 404 error handler
-app.use(errorHandler);  // error handler
+app.get("/api/config/paypal", (req, res) =>
+  res.send({ clientId: process.env.PAYPAL_CLIENT_ID })
+);
+
+app.use(notFound); // 404 error handler
+app.use(errorHandler); // error handler
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
